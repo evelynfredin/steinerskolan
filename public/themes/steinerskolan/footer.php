3759b7fcada63786cda24242977a2457fec9cecf
evelynfredin <evefre2001@skola.goteborg.se>
2021-09-28 15:15:22 +0200

    Initial setup for header with dummy hero img

--- public/themes/steinerskolan/footer.php
+++ public/themes/steinerskolan/footer.php
@@ -1,4 +1,7 @@
   <script src="<?= get_theme_file_uri('assets/app.js') ?>" async></script>
+  <script src="<?= get_theme_file_uri('assets/script.js') ?>" async></script>
+
   <?php wp_footer(); ?>
-</body>
-</html>
+  </body>
+
+  </html>
