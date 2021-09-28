bc4fbdf6b70c0fdf151941971de2b492f95a741f
evelynfredin <evefre2001@skola.goteborg.se>
2021-09-28 15:44:50 +0200

    Custom CSS and Scripts files added

---
+++ public/themes/steinerskolan/assets/script.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector("#menuBtn");
+const menuList = document.querySelector("#menuList");
+const closeMenu = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+</svg>
+`;
+
+const openMenu = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+</svg>`;
+
+menuBtn.addEventListener("click", () => {
+  if (menuList.classList.contains("hidden")) {
+    menuList.classList.remove("hidden");
+    menuBtn.innerHTML = closeMenu;
+  } else {
+    menuList.classList.add("hidden");
+    menuBtn.innerHTML = openMenu;
+  }
+});
