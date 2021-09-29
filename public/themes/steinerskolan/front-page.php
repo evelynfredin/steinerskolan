48401cb3e6303ef12dcc8ae2e461a529fa790981
evelynfredin <evefre2001@skola.goteborg.se>
2021-09-29 10:33:10 +0200

    Added directory for static images :octocat:

--- public/themes/steinerskolan/front-page.php
+++ public/themes/steinerskolan/front-page.php
@@ -2,8 +2,17 @@
 
 <main role="main">
     <section class="flex justify-center welcome">
-        <div class="w-[154px] h-10 bg-gray-400 self-end mb-16"></div>
+        <div class="w-[154px] h-10 bg-white self-end mb-16"></div>
     </section>
+    <?php if (have_posts()) : ?>
+
+        <?php while (have_posts()) : the_post(); ?>
+
+            <?php the_content(); ?>
+
+        <?php endwhile; ?>
+
+    <?php endif; ?>
 </main>
 
 <?php get_footer(); ?>
