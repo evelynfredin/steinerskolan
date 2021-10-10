d13fed43ec8265654a2426fb3432c3a229419281
carolinahagman <hagmancarolina@gmail.com>
2021-10-10 21:26:07 +0200

    Add custom fields to footer

--- public/themes/steinerskolan/fields/options.php
+++ public/themes/steinerskolan/fields/options.php
@@ -13,7 +13,9 @@ if (function_exists('acf_add_options_page')) {
         'redirect'        => false
     ]);
 }
-
+/*
+* Header options
+*/
 if (function_exists('acf_add_local_field_group')) {
     acf_add_local_field_group([
         'key' => 'header_fields',
@@ -63,6 +65,98 @@ if (function_exists('acf_add_local_field_group')) {
                 'wrapper' => [
                     'width' => 33,
                 ],
+            ],
+        ],
+        'location' => [
+            [
+                [
+                    'param' => 'options_page',
+                    'operator' => '==',
+                    'value' => 'theme-general-settings',
+                ],
+            ],
+        ],
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ]);
+}
+
+
+/*
+* Footer options
+*/
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group([
+        'key' => 'footer_fields',
+        'title' => 'Footer',
+        'fields' => [
+            [
+                'key' => 'footer_logo',
+                'label' => 'Footer Logo',
+                'name' => 'footer_logo',
+                'type' => 'image',
+                'instructions' => 'Add a logo to your footer. Preferable height: 210px',
+                'required' => 1,
+                'conditional_logic' => 0,
+                'wrapper' => [
+                    'width' => '100',
+                ],
+                'return_format' => 'url',
+                'preview_size' => 'thumbnail',
+                'library' => 'all',
+            ],
+            [
+                'key' => 'subheader_1',
+                'name' => 'subheader_1',
+                'label' => 'Subheader 1',
+                'type' => 'text',
+                'instructions' => 'Set the text for subheader 1',
+                
+            ],
+            [
+                'key' => 'subheader_2',
+                'name' => 'subheader_2',
+                'label' => 'Subheader 2',
+                'type' => 'text',
+                'instructions' => 'Set the text for subheader 2',
+                
+            ],
+            [
+                'key' => 'details_1',
+                'name' => 'details_1',
+                'label' => 'Details 1',
+                'type' => 'textarea',
+                'instructions' => 'Add information for subheader 1',
+                
+            ],
+            [
+                'key' => 'details_2',
+                'name' => 'details_2',
+                'label' => 'Details 2',
+                'type' => 'textarea',
+                'instructions' => 'Add information for subheader 2',
+                
+            ],
+            [
+                'key' => 'facebook_link',
+                'name' => 'facebook_link',
+                'label' => 'Facebook Link',
+                'type' => 'link',
+                'instructions' => 'Link for Facebook',
+            ],
+            [
+                'key' => 'instagram_link',
+                'name' => 'instagram_link',
+                'label' => 'Instagram Link',
+                'type' => 'link',
+                'instructions' => 'Link for Instagram',
             ],
         ],
         'location' => [
--- public/themes/steinerskolan/footer.php
+++ public/themes/steinerskolan/footer.php
@@ -2,30 +2,36 @@
 <script src="<?= get_theme_file_uri('assets/script.js') ?>" async></script>
 
 <?php wp_footer(); ?>
-<footer class="bg-gray-400 flex flex-col items-center w-full h-1/3 text-center">
-    <div class="w-full p-5">
-        <ul class="flex justify-around w-full ">
-            <li>
-                <div class="w-10 h-10 bg-gray-800 mb-2"></div>
-                <div class="w-10 h-3 bg-gray-800"></div>
-            </li>
-            <li>
-                <div class="w-10 h-10 bg-gray-800 mb-2"></div>
-                <div class="w-10 h-3 bg-gray-800"></div>
-            </li>
-            <li>
-                <div class="w-10 h-10 bg-gray-800 mb-2"></div>
-                <div class="w-10 h-3 bg-gray-800"></div>
-            </li>
+<?php $menu = wp_get_nav_menu_items('footer-menu'); 
 
-        </ul>
-        <div class="flex justify-evenly mt-3">
-            <p>textruta</p>
-            <p>textruta</p>
-        </div>
-    </div>
-    <div class=" h-[80px] w-full bg-gray-600 flex  items-center  justify-center"><img class="mr-3" src="http://159.89.96.200/wp-content/themes/steinerskolan/assets/img/Facebook.svg" alt="facebook"><img class="ml-3" src="http://159.89.96.200/wp-content/themes/steinerskolan/assets/img/Instagram.svg" alt="instagram"></div>
-</footer>
+?>
+<img src="<?php the_field('footer_logo', 'option') ?>" alt="Rudolf Steinerskolan" class="md:h-[55px] h-[40px] w-auto">
+  <?php if (get_field('subheader_1', 'option')) : ?>
+                    <p class="text-2xl font-black md:text-4xl mb-8"><?php the_field('subheader_1', 'option'); ?></p>
+                <?php endif; ?>
+                <?php if (get_field('details_1', 'option')) : ?>
+                    <p class=""><?php the_field('details_1', 'option'); ?></p>
+                <?php endif; ?>
+
+                <?php if (get_field('subheader_2', 'option')) : ?>
+                    <p class="text-2xl font-black md:text-4xl mb-8"><?php the_field('subheader_2', 'option'); ?></p>
+                <?php endif; ?>
+                <?php if (get_field('details_2', 'option')) : ?>
+                    <p class=""><?php the_field('details_2', 'option'); ?></p>
+                <?php endif; ?>
+
+                <a href="<?php get_field('facebook_link', 'option')?>"><img src="" alt="facebook logo" srcset=""></a>
+                <a href="<?php get_field('instagram_link', 'option')?>"><img src="" alt="instagram logo" srcset=""></a>
+
+
+ <?php foreach ($menu as $menuItem) : ?>
+    <li>
+        <a class="" href="<?= $menuItem->url ?>">
+            <?= $menuItem->title; ?>
+        </a>
+    </li>
+<?php endforeach; ?>
+
 
 </body>
 
