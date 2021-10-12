1bf10ce2028b923caccda57a27273e6fe1b71a6f
carolinahagman <hagmancarolina@gmail.com>
2021-10-12 17:08:07 +0200

    Update about school page template

--- public/themes/steinerskolan/about-school.php
+++ public/themes/steinerskolan/about-school.php
@@ -5,8 +5,8 @@
 
 get_header(); ?>
 
-<header class="flex justify-center w-full mt-20 mb-48">
-<h1 class=""><?php the_title(); ?></h1>
+<header class="flex justify-center w-full mt-20 mb-24">
+<h1 class="text-schoolGreen text-4xl sans"><?php the_title(); ?></h1>
 </header>
 
 <?php
@@ -24,16 +24,17 @@ if ( $arr_posts->have_posts() ) :
     while ( $arr_posts->have_posts() ) :
         $arr_posts->the_post();
         ?> 
-       <div class="flex justify-center w-screen relative overflow-x-hidden">
+       <div class="flex justify-center items-center w-screen relative overflow-x-hidden mb-24">
            
        <a href="<?php the_permalink(); ?>">
-       <article class="mb-10" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+       <article class="pb-10 max-w-3xl" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <?php
             if ( has_post_thumbnail() ) :
                 the_post_thumbnail();
             endif;
             ?>
-            <button class="absolute top-2/3 py-3 text-white text-center bg-gray-600 w-full max-w-[800px] text-2xl font-bold"><?php the_title(); ?></button>
+            <div class="absolute bottom-1 bg-secondaryBtn secondary-button-position flex justify-center w-[267px] h-[64px]"><p class="py-3 text-schoolRed waldorf text-center text-2xl font-bold"><?php the_title(); ?></p></div>
+     
             
                </a>
         </article>
