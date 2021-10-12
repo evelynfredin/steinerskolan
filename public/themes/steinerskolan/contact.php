23ff7bd31ddd2d8f63cd90fc12f58323093b0786
carolinahagman <hagmancarolina@gmail.com>
2021-10-12 17:12:15 +0200

    Update contact page template

--- public/themes/steinerskolan/contact.php
+++ public/themes/steinerskolan/contact.php
@@ -5,9 +5,9 @@
 
 get_header(); ?>
 <main class="flex flex-col items-center w-full mb-40"> 
-<header class=" mt-20 mb-48">
-<h1 class=""><?php the_title(); ?></h1>
+<header class="flex justify-center w-full mt-20 mb-24">
+<h1 class="text-schoolGreen text-4xl sans"><?php the_title(); ?></h1>
 </header>
-<div class="max-w-[800px] text-center ml-1 mr-1">  <?php the_content(); ?> </div>
+<div class="max-w-[800px] text-center ml-1 mr-1 text-schoolGreen">  <?php the_content(); ?> </div>
 </main>
 <?php get_footer() ?>
