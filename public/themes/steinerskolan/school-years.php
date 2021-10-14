03408f34c5ed6e3e4a685b6346e0489fc09fd652
carolinahagman <hagmancarolina@gmail.com>
2021-10-14 08:47:18 +0200

    Update font size on heading

--- public/themes/steinerskolan/school-years.php
+++ public/themes/steinerskolan/school-years.php
@@ -6,7 +6,7 @@
 get_header(); ?>
 
 <header class="flex justify-center w-full mt-20 mb-48">
-<h1 class="text-schoolGreen text-4xl sans"><?php the_title(); ?></h1>
+<h1 class="text-schoolGreen text-4xl lg:text-7xl sans"><?php the_title(); ?></h1>
 </header>
 
 <?php if (have_posts()) : ?>
