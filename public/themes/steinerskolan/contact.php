34cf0b7897cc761295fac6157a18b68f54fa5710
carolinahagman <hagmancarolina@gmail.com>
2021-10-14 09:01:52 +0200

    Fix blocks to stay on toggle

--- public/themes/steinerskolan/contact.php
+++ public/themes/steinerskolan/contact.php
@@ -6,7 +6,7 @@
 get_header(); ?>
 <main class="flex flex-col items-center w-full mb-40"> 
 <header class="flex justify-center w-full mt-20 mb-24">
-<h1 class="text-schoolGreen text-4xl sans"><?php the_title(); ?></h1>
+<h1 class="text-schoolGreen text-4xl lg:text-7xl sans"><?php the_title(); ?></h1>
 </header>
 <div class="max-w-[800px] text-center ml-1 mr-1 text-schoolGreen">  <?php the_content(); ?> </div>
 </main>
